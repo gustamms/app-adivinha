d143d2eb154add3d2ebe78834efae9591663050f
Gustavo Mendes <gustamms@hotmail.com>
2020-04-16 19:43:18 -0300

    adicionado apostas de dois jogadores

--- app/src/main/java/com/example/adivinha/MainActivity.java
+++ app/src/main/java/com/example/adivinha/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botaoJogar;
     private TextView textoResultado;
+    private EditText aposta1;
+    private EditText aposta2;
+    private TextView resultadoAposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,32 @@ public class MainActivity extends AppCompatActivity {
 
         botaoJogar = (Button) findViewById(R.id.botaoJogarId);
         textoResultado = (TextView) findViewById(R.id.texto);
-
-        //textoResultado.setText("Alterado com Sucesso!");
+        aposta1 = (EditText) findViewById(R.id.inputAposta1);
+        aposta2 = (EditText) findViewById(R.id.inputAposta2);
+        resultadoAposta = (TextView) findViewById(R.id.resultadoAposta);
 
         botaoJogar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Random randomico = new Random();
                 int numeroAleatorio = randomico.nextInt(10);
-                textoResultado.setText("Número = " + (numeroAleatorio+1));
+                if(!aposta1.getText().toString().isEmpty() && !aposta2.getText().toString().isEmpty()){
+                    textoResultado.setText("Número = " + (numeroAleatorio+1));
+                    int resultAposta1 = Integer.parseInt(aposta1.getText().toString());
+                    int resultAposta2 = Integer.parseInt(aposta2.getText().toString());
+
+                    int resultadoAposta1 = (numeroAleatorio+1) - resultAposta1;
+                    int resultadoAposta2 = (numeroAleatorio+1) - resultAposta2;
+                    if(resultadoAposta1 < resultadoAposta2){
+                        resultadoAposta.setText("Jogador 1 ganhou");
+                    }else if(resultadoAposta1 > resultadoAposta2){
+                        resultadoAposta.setText("Jogador 2 ganhou");
+                    }else{
+                        resultadoAposta.setText("Ambos ganharam");
+                    }
+                }else{
+                    textoResultado.setText("Número = " + (numeroAleatorio+1));
+                }
             }
         });
     }
