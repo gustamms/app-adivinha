ea92bbdfd05d7d8275d659f31ff8f772a4d665b7
Gustavo Mendes <gustamms@hotmail.com>
2020-04-16 13:39:03 -0300

    ideia inicial

---
+++ app/src/main/java/com/example/adivinha/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.adivinha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView) findViewById(R.id.texto);
+
+        //textoResultado.setText("Alterado com Sucesso!");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+                textoResultado.setText("Número = " + (numeroAleatorio+1));
+            }
+        });
+    }
+}
